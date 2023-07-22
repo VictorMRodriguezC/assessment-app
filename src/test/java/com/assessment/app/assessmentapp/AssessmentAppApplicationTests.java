3309145ae6a512e2408e1968e102ef4e2e478bb7
VictorRodriguez <vicrodriguezc69@gmail.com>
2023-07-22 17:56:09 -0500

    Initial Proyect Structure

---
+++ src/test/java/com/assessment/app/assessmentapp/AssessmentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.app.assessmentapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
